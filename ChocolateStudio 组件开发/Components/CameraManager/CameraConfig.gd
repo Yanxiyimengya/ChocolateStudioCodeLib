d50abafb4c7a780591c4de4b87dcd4a51871f9f1
Yanxiyimengym <193446537@qq.com>
2023-10-22 14:11:20 +0800

    main

--- ChocolateStudio 组件开发/Components/CameraManager/CameraConfig.gd
+++
@@ -1,7 +0,0 @@
-extends Resource;
-class_name CameraConfig;
-# 摄像机的配置数据
-
-@export var size: Vector2i = Vector2i(1920, 1080);
-@export var angle: float = 0;
-@export var position: Vector2 = Vector2.ZERO;
